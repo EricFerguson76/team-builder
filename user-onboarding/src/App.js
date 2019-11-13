506548e3eaa2592fe29ebabe866f649e876bf60a
EricFerguson76 <turtle0888@gmail.com>
2019-11-13 16:54:41 -0600

    Added to app component

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Form from './components/Form';
+import TeamList from './components/TeamLIst';
 import './App.css';
 
 function App() {
+
+  const [members, setMembers] = useState([{
+    name: '',
+    email: '',
+    role: ''
+  }])
+
+  const submit = person => {
+    setMembers([...members, person])
+  }
+
+
   return (
     <div className="App">
+      <h1>Team Members</h1>
 
+      <TeamList teamMembers={members} />
     </div>
   );
 }
